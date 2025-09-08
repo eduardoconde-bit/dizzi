3cd842419b3cf97869da590f8cabc98e572f5090
Eduardo Conde <luiseduardojp8@gmail.com>
2025-09-08 11:13:57 -0300

    Update token issuance in cookie handling route: include user token and add POST data handling

--- app/Routes/cookie.php
+++ app/Routes/cookie.php
@@ -5,7 +5,9 @@ require __DIR__ . './../../vendor/autoload.php';
 use Dizzi\Models\User;
 use Dizzi\Services\TokenService;
 
-TokenService::issueToken(new User('eduardo'), 3600, false);
+$data = $_POST ?? null;
+
+TokenService::issueToken(new User('eduardo', '102030abc'));
 header("Content-Type: application/json");
 echo json_encode([
     "cookies" => $_COOKIE
